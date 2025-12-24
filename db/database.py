55893e9e8646e4c267dd4c90aed8bfc108c23ae5
Ivan Blagochevskiy <blagochevskyivan@gmail.com>
2025-12-24 15:45:53 +0300

    Добавил значения классу exercises

--- db/database.py
+++ db/database.py
@@ -1,6 +1,6 @@
 from typing import AsyncGenerator
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker
 from contextlib import asynccontextmanager
 
 from config.cp_config import DATABASE_URL, DATABASE_ECHO
--- db/models.py
+++ db/models.py
@@ -17,4 +17,9 @@ class User(Base):
     
 class Exercise(Base):
     __tablename__ = "exercises"
+
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    name = Column(String, nullable=False)
+
+    user = relationship("User", back_populates="exercises")
