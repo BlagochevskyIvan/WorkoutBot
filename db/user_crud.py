03558bc0d9efb42de9d0c7da2fecc475c7010b21
Ivan Blagochevskiy <blagochevskyivan@gmail.com>
2025-12-25 15:59:24 +0300

    Созданы функции добавления стажа, даты рождения, места тренировок в бд

--- db/user_crud.py
+++ db/user_crud.py
@@ -29,4 +29,33 @@ async def add_gender(telegram_id: int, gender) -> None:
             .where(User.telegram_id == telegram_id)
         )
         await session.commit()
-    
+
+async def add_expirience(telegram_id: int, experience):
+    """добавляет опыт пользователя"""
+    async with get_session() as session:
+        await session.execute(
+            update(User)
+            .values(experience = experience)
+            .where(User.telegram_id == telegram_id )
+        )
+        await session.commit()
+
+async def add_birth_date(telegram_id: int, birth_date):
+    """Добавляет дату рождения пользователя"""
+    async with get_session() as session:
+        await session.execute(
+            update(User)
+            .values(birth_date = birth_date)
+            .where(User.telegram_id == telegram_id)
+        )
+        await session.commit()
+
+async def add_place(telegram_id: int, place):
+    """Добавляет место тренировок пользователя"""
+    async with get_session() as session:
+        await session.execute(
+            update(User)
+            .values(place = place)
+            .where(User.telegram_id == telegram_id)
+        )
+        await session.commit()
--- handlers/profile.py
+++ handlers/profile.py
@@ -8,7 +8,7 @@ from telegram.ext import (
 )
 
 from config.states import MENU, GET_DATE, PROFILE
-from db.user_crud import add_gender
+from db.user_crud import add_gender, add_birth_date, add_expirience, add_place
 
 async def get_gender(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     query = update.callback_query
@@ -29,6 +29,7 @@ async def get_experience(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
     query = update.callback_query
     await query.answer()
     experience = query.data
+    await add_expirience(telegram_id=update.effective_user.id, experience=experience)
     context.user_data["experience"] = experience
     keyboard = [[InlineKeyboardButton("Квартира", callback_data='flat'),
                  InlineKeyboardButton("Тренажёрный зал", callback_data='gym')]]
@@ -42,6 +43,7 @@ async def get_place(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     query = update.callback_query
     await query.answer()
     place = query.data
+    await add_place(telegram_id=update.effective_user.id, place=place)
     context.user_data["place"] = place
     await query.edit_message_text(
         text="Место тренировок установлено. \nТеперь введите вашу дату рождения в формате ДД.ММ.ГГГГ (например, 01.01.2001)",
@@ -59,7 +61,8 @@ async def get_date(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
             text="Некорректный формат даты. Пожалуйста, введите дату в формате ДД.ММ.ГГГГ (например, 01.01.2001).",
         )
         return GET_DATE
-
+    await add_birth_date(telegram_id=tg_user.id, birth_date=date_str)
+    context.user_data["birth_date"] = date_str
     await context.bot.send_message(
         chat_id=tg_user.id,
         text="Дата сохранена. Ваш профиль успешно создан!\nИспользуйте главное меню для навигации.",
